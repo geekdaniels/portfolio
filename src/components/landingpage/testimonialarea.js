5101c54a932b1b9c2fe750d6fcc2a6213e2e4a66
Olufemi Daniel <oladotun.olufemi@yahoo.com>
2020-02-03 14:47:54 +0100

    window update fix

--- src/components/landingpage/testimonialarea.js
+++ src/components/landingpage/testimonialarea.js
@@ -1,5 +1,7 @@
 import React from "react"
-import OwlCarousel from 'react-owl-carousel';
+import Loadable from '@loadable/component'
+
+const OwlCarousel = Loadable(() => import('react-owl-carousel'));
 
 import "owl.carousel/dist/assets/owl.carousel.css"
 import "owl.carousel/dist/assets/owl.theme.default.css"
